1e197b8feb1ad297f2d17b31fa43202c991f1502
ElieDaher07 <143806470+ElieDaher07@users.noreply.github.com>
2025-10-07 15:58:54 -0400

    Bouton key + atom, sans pba

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,43 @@
 #include <Arduino.h>
 
-// put function declarations here:
-int myFunction(int, int);
+#include <FastLED.h>
+
+#define BROCHE_BOUTON_KEY 32
+#define BROCHE_BOUTON_KEY_LUMIERE 26
+
+#define BROCHE_BOUTON_ATOM 39
+#define BROCHE_BOUTON_ATOM_LUMIERE 27
+
+CRGB pixelKey;
+CRGB pixelAtom;
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  FastLED.addLeds< WS2812, BROCHE_BOUTON_KEY_LUMIERE , GRB >(&pixelKey, 1); 
+  FastLED.addLeds< WS2812,  BROCHE_BOUTON_ATOM_LUMIERE , GRB >(&pixelAtom, 1); 
+  pinMode( BROCHE_BOUTON_KEY , INPUT );
+  pinMode( BROCHE_BOUTON_ATOM , INPUT );
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-}
+void loop() { 
+  int maLectureKey = digitalRead( BROCHE_BOUTON_KEY );
+  int maLectureAtom = digitalRead( BROCHE_BOUTON_ATOM);
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+
+  if (maLectureAtom == 0) {
+    pixelAtom = CRGB(0,0,127);
+  } else {
+    pixelAtom = CRGB (0, 0, 0);
+  }
+  
+
+  if (maLectureKey == 0 ) {
+    pixelKey = CRGB(127,255,0);
+  } else {
+    pixelKey = CRGB(0,0,0);
+  }
+
+  delay(20);
+
+
+  FastLED.show();
 }
